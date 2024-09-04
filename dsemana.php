a6de0c162ad64254480e923bc08325340a2d5656
cldcantoflores <cld.cantoflores@gmail.com>
2024-09-04 16:00:40 -0400

    función para formatear fecha al español
    como set locate no me funcionó, crear la propia función

--- dsemana.php
+++ dsemana.php
@@ -1,14 +1,24 @@
+<meta charset='utf-8'>
+
 <?php
 
-function nombreDiaSemana($fecha){
-   echo  $dia = date('w', strtotime($fecha));
+function FechaTexto($fecha){
+   $dia = date('w', strtotime($fecha));
+   $diaF = date('d', strtotime($fecha));
+   $mes = date('n', strtotime($fecha));
+   $ano = date('Y', strtotime($fecha));
+
    
         
     $nombresDias = array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" );
+    $nombresMEs = array("","Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
     
     $diaS = $nombresDias[$dia];
-    return $diaS;
+    $mesS = $nombresMEs[$mes];
+    $textoFecha = $diaS." ".$diaF∫." del ".$mesS." del ".$ano;
+
+    return $textoFecha;
     }
 
-    echo nombreDiaSemana('2024-08-11');
+    echo FechaTexto('2024-08-11');
 ?>
